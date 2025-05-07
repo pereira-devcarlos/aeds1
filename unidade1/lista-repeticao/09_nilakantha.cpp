6f5e047b65a24bafb9109c33dab12a5be36faca1
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-05-07 19:15:54 -0300

    Adicionando comentários

--- unidade1/lista-repeticao/09_nilakantha.cpp
+++ unidade1/lista-repeticao/09_nilakantha.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-int main(){
-    int i, sinal, t;
-    float pi, den, soma;
+int main() {
+    int termos, i = 1, sinal = 1;
+    float pi = 3.0, soma = 0.0;
+    int a = 2; // Primeiro número do denominador
 
-    t = 2;
-    den = 2;
-    sinal = -1;
-    pi = 3;
-    i = 1;
+    cout << "Digite o número de termos para a aproximação de PI: ";
+    cin >> termos;
 
-    while (i<3){
-        t += 2;
-        den *= t * (t-1) ;
-        sinal = -sinal;
+    while (i <= termos) {
+        // Cada termo é: 4 / (a * (a+1) * (a+2))
+        float termo = (4.0 / (a * (a + 1) * (a + 2))) * sinal;
 
-        soma += (sinal * 4) / den;
+        soma += termo;       // Soma o termo com o sinal correto
+        sinal = -sinal;      // Alterna o sinal (+/-)
+        a += 2;              // Avança para o próximo trio de números
         i++;
     }
-    pi = 3 + soma;
 
-    cout << "O denominador e: " << den << endl;
-    cout << "O valor de PI: " << pi << endl;
+    pi += soma; // Soma os termos alternados à base 3
+
+    cout << "Valor aproximado de PI com " << termos << " termos: " << pi << endl;
+
     return 0;
 }
