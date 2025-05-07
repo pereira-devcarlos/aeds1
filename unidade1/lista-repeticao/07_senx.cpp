0c658aaefce1e5c3921dba5e9e27b03325baab6b
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-05-07 19:08:49 -0300

    Adicionei os comentários

--- unidade1/lista-repeticao/07_senx.cpp
+++ unidade1/lista-repeticao/07_senx.cpp
@@ -1,46 +1,48 @@
 /* 
  * File:   main.cpp
  * Author: 2024.2.08.001
- *
  * Created on 30 de abril de 2025, 09:05
  */
 
-#include <cstdlib>
-#include <iostream>
+#include <iostream>  // Biblioteca para entrada e saída de dados
 
-using namespace std;
+using namespace std; 
 
-/*
- * 
- */
 int main() {
+    // Declaração de variáveis
     int i, termo, den, fat, sinal;
     float x, num, seno;
 
+    // Solicita ao usuário o valor de x em radianos
     cout << "Digite o valor de x(em radianos): ";
     cin >> x;
+
+    // Solicita ao usuário o número de termos para a aproximação
     cout << "Digite o número de termos: ";
     cin >> termo;
 
-    num = x;
-    sinal = 1;
-    fat = 1;
-    den = 1;
-    seno = x;
-    i = 1;
+    // Inicializações:
+    num = x;      // Primeiro termo do numerador é x
+    sinal = 1;    // O primeiro termo tem sinal positivo
+    fat = 1;      // Fator para cálculo do fatorial (usado para controle do denominador)
+    den = 1;      // Denominador começa com 1 (1! = 1)
+    seno = x;     // O primeiro termo da série de Taylor é x
+    i = 1;        // Inicia o contador do laço
 
+    // Loop para calcular os termos seguintes da série de Taylor para o seno
     while (i < termo) {
-        fat += 2;
-        den *= fat * (fat - 1);
-        
-        sinal = -sinal; 
-        num *= x*x;
-        seno += (sinal * num) / den;
-        i++;
+        fat += 2;                        // Próximo expoente ímpar (3, 5, 7, ...)
+        den *= fat * (fat - 1);          // Calcula o novo fatorial: fat! = (fat-1)! * fat
+        sinal = -sinal;                  // Alterna o sinal (+, -, +, ...)
+        num *= x * x;                    // Calcula o novo numerador: x^(2i+1)
+        seno += (sinal * num) / den;     // Adiciona ou subtrai o termo à soma do seno
+        i++;                             // Incrementa o número de termos usados
     }
 
+    // Exibe o resultado aproximado do seno de x
     cout << "O valor aproximado de seno de x: " << seno << endl;
-    
+
     return 0;
 }
+
 
