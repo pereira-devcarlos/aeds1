de7ffc526e5e86eb00ce8477408bfec87a61f168
Carlos Eduardo Pereira <contato.devcarlos@gmail.com>
2025-05-10 02:06:18 -0300

    Adicionei a leitura de dados, e entrada

--- unidade2/project-quiz/main.cpp
+++ unidade2/project-quiz/main.cpp
@@ -8,21 +8,77 @@ using namespace std;
 
 int main(){
     const int TAM=5; // Constante para definir os tamanhos dos vetores
+    int menu;
     
     
 
     //Valores Aleatórios
     srand(time(NULL));  //Gerar seed de acordo com as horas do meu pc
-    //int random = rand() % 10; //Gerador de números inteiros aleatórios
     
     //Definindo a entrada das perguntas de um arquivo texto
     ifstream arquivoP("perguntas.txt");
 
     //Declaração dos vetores
-    string perguntas[1] = "Qual e o comando para imprimir no console em C++?";
+    string perguntas[TAM];
     string opcoes[TAM][4];
-    
-    cout << endl << perguntas[1] << endl;
-    
+    string correta[TAM];
+    string resposta[TAM];
+
+
+    menu = 2;
+    while (menu!=3){
+            // Menu Inicial
+        cout << "\n\tQuiz de Programacao" << endl;
+        cout << "[1] Jogar" << endl << "[2] Ranking" << endl << "[3] Sair" << endl;
+        cout << "Digite a opcao que voce deseja: ";
+        cin >> menu;
+
+        // Validar a escolha das opcoes
+        while (menu<1 || menu>3){
+            cout << "\nErro: opcao invalida!!!" << endl;
+            cout << "Digite a opcao que voce deseja: ";
+            cin >> menu;
+        }
+
+        if (menu == 1){
+            // Coletando os dados
+            for (int i = 0; i < TAM; i++){
+                getline(arquivoP, perguntas[i]);     // Dados das perguntas
+                for (int j = 0; j < 4; j++){
+                    getline(arquivoP, opcoes[i][j]); // Dados das alternativas
+                }
+                getline(arquivoP, correta[i]);
+            }
+            
+            // Verificando as perguntas e alternativas
+            cout << endl;
+            for (int i = 0; i < TAM; i++){
+                int random = rand() % TAM; // Gerar pergunta aleatória
+
+                cout << i+1 << "-" << perguntas[random] << endl;
+                for (int j = 0; j < 4; j++){
+                    cout << opcoes[random][j] << endl;
+                }
+                cout << "R: ";
+                cin >> resposta[random];
+                if(resposta[random]==correta[random]){
+                    cout << "\nParabens voce acertou!!!" << endl;
+                }
+            }
+        }
+        else if (menu == 2){
+            cout << "\nVoce esta em terceiro lugar no ranking!!!" << endl;
+            cout << "\nDeseja continuar no jogo [1]-Sim ou [2]-Nao: ";
+            cin >> menu;
+            if(menu == 2){
+                menu = 3;
+                cout << "\nObrigado por acessar meu jogo, volte sempre!!!" << endl;
+            }
+        }
+        else{
+            cout << "\nVolte sempre, basta executar o jogo novamente!";
+        }
+    }
+
     return 0;
 }
