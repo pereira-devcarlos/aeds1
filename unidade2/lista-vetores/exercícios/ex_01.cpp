03c49ce5bf4e9220803b91a7ab09f0c40eb032b0
Carlos Eduardo Pereira <contato.devcarlos@gmail.com>
2025-05-19 20:53:17 -0300

    feat: declarando as variáveis e vetores

--- unidade2/lista-vetores/exercícios/ex_01.cpp
+++ unidade2/lista-vetores/exercícios/ex_01.cpp
@@ -2,17 +2,11 @@
 
 using namespace std;
 
-int main(){
-    int numbers[10];
-
-    for (int i = 0; i < 10; i++){
-        cin >> numbers[i];
-    }
-    
-    for (int i = 0; i < 10; i++){
-        cout << "Vetor " << i+1 << " " << numbers[i] << endl; 
-    }
-    
+int main() {
+    const int TAM = 10;
+    int vetor[TAM] = {2, 5, 2, 8, 5, 9, 8, 3, 3, 7}; // Exemplo de vetor com repetições
+    int unicos[TAM]; // Vetor para armazenar apenas valores únicos
+    int qtd_unicos = 0;
 
     return 0;
 }
