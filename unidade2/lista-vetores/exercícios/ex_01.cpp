0bd193e6ad5460e959086df32deca0735d61e814
Carlos Eduardo Pereira <contato.devcarlos@gmail.com>
2025-05-19 20:54:20 -0300

    feat: percorrer cada elemento do vetor

--- unidade2/lista-vetores/exercícios/ex_01.cpp
+++ unidade2/lista-vetores/exercícios/ex_01.cpp
@@ -8,5 +8,9 @@ int main() {
     int unicos[TAM]; // Vetor para armazenar apenas valores únicos
     int qtd_unicos = 0;
 
+    // Percorre cada elemento do vetor original
+    for (int i = 0; i < TAM; i++) {
+        int contador = 0; // Conta quantas vezes vetor[i] já apareceu em unicos
+
     return 0;
 }
