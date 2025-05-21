df7eb8e3fb6d2d4b8084166f8dcf397274d367e6
Carlos Eduardo Pereira <contato.devcarlos@gmail.com>
2025-05-21 09:05:14 -0300

    refactor: retirando as condicionais nos for's

--- unidade2/aulas/04_char.cpp
+++ unidade2/aulas/04_char.cpp
@@ -16,6 +16,7 @@ int main() {
     char nome[30], sobreNome[30], nameComplete[60];
     int i, j;
     
+    // Preenchendo os valores com meu nome
     nome[0] = 'C';
     nome[1] = 'a';
     nome[2] = 'r';
@@ -24,8 +25,10 @@ int main() {
     nome[5] = 's';
     nome[6] = '\0';
     
+    // Exibindo os valores com meu nome    
     printf("\n Nome: %s\n", nome);
     
+    // Preenchendo os valores com meu sobrenome
     sobreNome[0] = 'E';
     sobreNome[1] = 'd';
     sobreNome[2] = 'u';
@@ -35,25 +38,24 @@ int main() {
     sobreNome[6] = 'o';
     sobreNome[7] = '\0';
     
+    // Exibindo os valores com meu sobrenome   
     printf("\n Sobrenome: %s\n", sobreNome);
     
-    for(i=0; i<30; i++){
-        nameComplete[i] = nome[i];
-        if(nameComplete[i] == '\0'){
-            nameComplete[i] = ' ';
-            i=30 + 1;
-        }
-    }
-    j=0;
-    for(i=0; i<30;i++){
-        if(nameComplete[i]==' '){
-            i++;
-            for(j=0; j<30 ; j++, i++){
-                nameComplete[i] = sobreNome[j];
-            }
-        }
+    // Adicionando o Nome no Char nameComplete
+    for(i=0, j=0; nome[i]!='\0'; i++,j++){
+        nameComplete[j]= nome[i];
     }
     
+    nameComplete[j]=' '; //Adicionando o espaço após o nome
+    j++;
+    
+    // Adicionando o Sobrenome no Char nameComplete
+    for(i=0; sobreNome[i]!='\0'; i++,j++){
+        nameComplete[j]=sobreNome[i];
+    }
+    nameComplete[j]=' '; // Finalizando com espaço caso precise adicionar outro nome
+    
+    // Exibindo o nome completo
     printf("\n Nome Completo: %s\n", nameComplete);
     
     return 0;
