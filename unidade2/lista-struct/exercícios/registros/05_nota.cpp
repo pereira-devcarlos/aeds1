fa8ee8feedcaaaaf1a71b32c8084844c938b2f93
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-05-29 18:16:06 -0300

    feat: validar notas do usuario

--- unidade2/lista-struct/exercícios/registros/05_nota.cpp
+++ unidade2/lista-struct/exercícios/registros/05_nota.cpp
@@ -15,7 +15,7 @@ int main(){
     Aluno aluno[TAM]; // Declarando um array de Aluno
     int i; // Variável de controle para o loop
 
-    int opcao = 0; // Variável para opcao do menu
+    int opcao = -1; // Variável para opcao do menu
     float soma = 0.0; // Variável para armazenar a soma das notas
     float media; // Variável para armazenar a média das notas
 
@@ -24,10 +24,17 @@ int main(){
         cout << "\nDigite o nome do aluno " << i+1 << ": ";
         cin >> aluno[i].nome;
         cout << "Digite a nota do aluno " << i+1 << ": ";
-        cin >> aluno[i].nota; 
+        // Verificando se a nota do user é valida
+        do{
+            cin >> aluno[i].nota;
+            // Condições para aceitar somente notas de 0 a 10 
+            if(aluno[i].nota<0 || aluno[i].nota>10)
+                cout << "\nErro: nota invalida." << "\nDigite a nota do aluno " << i+1 << " novamente: ";
+        }while (aluno[i].nota>10 || aluno[i].nota < 0);
         soma += aluno[i].nota; // Somando as notas
     }
-
+    media = soma / TAM; // Calculando a média
+    
     // Exibir Menu
     while(opcao != 0){
         cout << "Menu de Opçoes:" << endl;
@@ -35,20 +42,33 @@ int main(){
         cout << "[2]-Exibir a media das notas da turma" << endl;
         cout << "[3]-Exibir dados dos alunos aprovados" << endl;
         cout << "[4]-Exibir dados dos alunos reprovados" << endl;
-    }
 
-    cout << "\nDados dos alunos:" << endl;
-    // Imprimindo os dados dos alunos
-    for (i = 0; i < TAM; i++){
-        cout << "Nome: " << aluno[i].nome << endl;
-        cout << "Nota: " << aluno[i].nota << endl;
-        cout << "------------------------" << endl;
+        cout << "Digite uma das opcao acima: ";
+        cin >> opcao;
+
+        switch (opcao){
+        case 1:
+            // Imprimir dados dos alunos
+            cout << "\nDados dos alunos:" << endl;
+            for (i = 0; i < TAM; i++){
+                cout << "Nome: " << aluno[i].nome << endl;
+                cout << "Nota: " << aluno[i].nota << endl;
+                cout << "------------------------" << endl;
+            }
+            break;
+        case 2:
+            // Exibindo a média das notas
+            cout << "A media das notas da turma e: " << media << endl;
+            cout << "------------------------" << endl;            
+            break;
+        case 3:
+            
+            break;
+        default:
+            cout << "Erro: opcao inexistente, porfavor escolha uma opcao valida!" << endl;
+            break;
+        }
     }
-    
-    media = soma / TAM; // Calculando a média
-    // Exibindo a média das notas
-    cout << "A media das notas da turma e: " << media << endl;
-    cout << "------------------------" << endl;
 
     return 0;
 }
