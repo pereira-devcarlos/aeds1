9d31402fbfdb07798a85b25cb53f99894aa87260
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-05-29 18:00:59 -0300

    feat: adicionando code do exercicio 4

---
+++ unidade2/lista-struct/exercícios/registros/05_nota.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+//5. Filtrando structs com base em uma condição
+//Usando a struct `Aluno`, leia os dados de 5 alunos. Em seguida, mostre apenas os alunos que tiveram nota maior ou igual a 7.
+
+struct Aluno{ // Definindo a struct Aluno
+    string nome;
+    float nota;
+};
+
+int main(){
+    const int TAM = 4;
+    Aluno aluno[TAM]; // Declarando um array de Aluno
+    int i; // Variável de controle para o loop
+
+    int opcao = 0; // Variável para opcao do menu
+    float soma = 0.0; // Variável para armazenar a soma das notas
+    float media; // Variável para armazenar a média das notas
+
+    // Lendo os dados dos alunos
+    for (i = 0; i < TAM; i++){
+        cout << "\nDigite o nome do aluno " << i+1 << ": ";
+        cin >> aluno[i].nome;
+        cout << "Digite a nota do aluno " << i+1 << ": ";
+        cin >> aluno[i].nota; 
+        soma += aluno[i].nota; // Somando as notas
+    }
+
+    cout << "\nDados dos alunos:" << endl;
+    // Imprimindo os dados dos alunos
+    for (i = 0; i < TAM; i++){
+        cout << "Nome: " << aluno[i].nome << endl;
+        cout << "Nota: " << aluno[i].nota << endl;
+        cout << "------------------------" << endl;
+    }
+    
+    media = soma / TAM; // Calculando a média
+    // Exibindo a média das notas
+    cout << "A media das notas da turma e: " << media << endl;
+    cout << "------------------------" << endl;
+
+    return 0;
+}
