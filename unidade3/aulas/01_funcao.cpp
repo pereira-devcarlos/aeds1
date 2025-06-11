7d9633b5b34f18904ca7ff1585534d4cc1435e8c
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-11 09:03:51 -0300

    feat: adicionar exibição de vetores ordenados e otimizar a função main

--- unidade3/aulas/01_funcao.cpp
+++ unidade3/aulas/01_funcao.cpp
@@ -19,18 +19,29 @@ void bubbleSort(int v[], int tam);
 int main() {
     // Declaração e inicialização do vetor de inteiros
     int num[]={10, 3, 5, 6, 7, 1, 9, 2, 4, 8};
+    int numCres[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int numDec[]={10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     
     // Exibe o vetor antes da ordenação
     cout << "\nVetor antes da ordenação:" << endl;
     listaVetor(num, 10);
+    listaVetor(numCres, 10);
+    listaVetor(numDec, 10);
     
     // Ordena o vetor usando bubble sort
-    cout << "\nVetor depois da ordenação" << endl;
     bubbleSort(num, 10);
-    // Exibe o vetor já ordenado
-    listaVetor(num, 10);
-    cout << endl;
+    bubbleSort(numCres, 10);
+    bubbleSort(numDec, 10);
 
+    // Exibe os vetores já ordenados
+    cout << "\nVetor depois da ordenação" << endl;
+    listaVetor(num, 10);
+
+    cout << "\nVetor crescente depois da ordenação" << endl;
+    listaVetor(numCres, 10);
+
+    cout << "\nVetor decrescente depois da ordenação" << endl;
+    listaVetor(numDec, 10);
     return 0;
 }
 
@@ -40,13 +51,14 @@ void listaVetor(int v[], int tam){
         // Imprime o índice (começando do 1) e o valor correspondente
         cout << i+1 << "º valor: " << v[i] << endl;
     }
+    cout << endl;
 }
 
 // Função que implementa o algoritmo de ordenação bubble sort
 void bubbleSort(int v[], int tam) {
     int i, j, aux;
     // Laço externo: controla o número de passagens pelo vetor
-    for (i = tam - 1; i > 1; i--){
+    for (i = tam - 1; i >= 1; i--){
         // Laço interno: percorre o vetor até a posição i
         for ( j = 0; j < i; j++){
             // Se o elemento atual é maior que o próximo, troca os dois
