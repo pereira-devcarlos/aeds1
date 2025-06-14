de02be4527267740bfdbf477529eeeea4796792a
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-14 17:35:37 -0300

    refactor: adiciona comentários explicativos para melhorar a legibilidade da função que encontra o maior valor em um vetor

--- unidade3/aulas/02_maior.c
+++ unidade3/aulas/02_maior.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
+// Função que retorna o maior valor de um vetor de inteiros
 int maior (int *vetor, int n){
-    int m = vetor[0];
+    int m = vetor[0]; // Inicializa 'm' com o primeiro elemento do vetor
     for (int i = 0; i < n; i++){
-        if(vetor[i]>m){
-            m = vetor[i];
+        if(vetor[i]>m){ // Se encontrar um valor maior que 'm'
+            m = vetor[i]; // Atualiza 'm' com esse valor
         }
     }
-    return m;
+    return m; // Retorna o maior valor encontrado
 }
 
 int main(){
+    // Declara e inicializa um vetor de 8 inteiros
     int v[8]={90, 3, 45, 6, 78, 19, 23, 9}, m;
+    // Chama a função 'maior' para encontrar o maior valor do vetor
     m = maior(v, 8);
+    // Exibe o maior valor encontrado
     printf("Maior = %d\n", m);
 
     return 0;
