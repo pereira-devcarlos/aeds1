a3884642fd5bf143cfce21c45410bef5cfa3b307
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-14 17:36:20 -0300

    refactor: adiciona comentários explicativos para melhorar a legibilidade da função de cálculo do MDC

--- unidade3/aulas/03_ex4P2(t).c
+++ unidade3/aulas/03_ex4P2(t).c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
+// Função que calcula o MDC (Máximo Divisor Comum) entre dois números inteiros
 int mdc(int a, int b) {
+    // Enquanto os números forem diferentes
     while (a != b) {
         if (a > b)
-            a = a - b;
+            a = a - b; // Subtrai b de a se a for maior
         else
-            b = b - a;
+            b = b - a; // Subtrai a de b se b for maior
     }
+    // Quando a == b, esse valor é o MDC
     return a;
 }
 
 int main(){
+    // Exibe o MDC entre 9 e 3
     printf("%d\n", mdc(9, 3));
     
     return 0;
