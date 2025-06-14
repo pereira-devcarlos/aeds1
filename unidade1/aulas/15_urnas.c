aa92e187e0920c8f9e6fef9e0c0e7d0b4af678e9
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-14 17:33:24 -0300

    feat: verifica e exibe a seção e a urna com o menor número de votos nulos

---
+++ unidade1/aulas/15_urnas.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){    
+    int secoes, urnas, nulos, secaoMe, urnaMe, secaoMa, urnaMa;
+    int menos = 100;
+    int maior = 0;
+
+    scanf("%d", &secoes);
+
+    for (int i = 0; i < secoes; i++){
+        scanf("%d", &urnas);
+
+        for (int j = 0; j < urnas; j++){
+            scanf("%d", &nulos);
+            if(nulos < menos){
+                menos = nulos;
+                secaoMe = i+1;
+                urnaMe = j+1;
+            }
+        }
+    }
+    printf("\nMenor numero de votos nulos foi a secao %d e a urna %d com apenas %d votos nulos\n", secaoMe, urnaMe, menos);
+
+    return 0;
+}
