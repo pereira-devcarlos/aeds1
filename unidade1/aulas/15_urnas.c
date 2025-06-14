52991ba36ea58ccff379a9feb65ad087ce9f4870
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-14 17:33:47 -0300

    feat: adiciona exibição do maior número de votos nulos por seção e urna

--- unidade1/aulas/15_urnas.c
+++ unidade1/aulas/15_urnas.c
@@ -17,9 +17,15 @@ int main(){
                 secaoMe = i+1;
                 urnaMe = j+1;
             }
+            if(nulos > maior){
+                maior = nulos;
+                secaoMa = i+1;
+                urnaMa = j+1;
+            }
         }
     }
     printf("\nMenor numero de votos nulos foi a secao %d e a urna %d com apenas %d votos nulos\n", secaoMe, urnaMe, menos);
+    printf("Maior numero de votos nulos foi a secao %d e a urna %d com o total de %d votos nulos\n", secaoMa, urnaMa, maior);
 
     return 0;
 }
