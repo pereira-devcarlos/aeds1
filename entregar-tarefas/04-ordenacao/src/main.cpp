301ad9beaacd162b83bdcd1b47c33e5417a17778
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-18 10:58:26 -0300

    refactor: melhorar a formatação e a legibilidade do código no main.cpp

--- entregar-tarefas/04-ordenacao/src/main.cpp
+++ entregar-tarefas/04-ordenacao/src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include "ordenacao.h"
+#include "ordenacao.h" // Inclua o cabeçalho das funções
 
 using namespace std;
 
-int main(){
-    
+int main() {
+    int vetor[] = {5, 2, 9, 1, 5, 6};
+    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
+
+    // Exemplo usando bubbleSort
+    bubbleSort(vetor, tamanho);
+
+    // Imprime o vetor ordenado
+    cout << "Vetor ordenado: ";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
