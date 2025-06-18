4d3068968f24e0a85e3a0f7151369170ed361605
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-18 09:51:47 -0300

    refactor: melhorar a legibilidade do código com comentários adicionais

--- unidade4/aulas/02_matrizes.cpp
+++ unidade4/aulas/02_matrizes.cpp
@@ -3,32 +3,44 @@
 using namespace std;
 
 int main(){
+    // Declara três ponteiros para inteiros
     int *p, *q, *r;
-    int m3[3][3];
-    int m1[3][3] = {
+
+    // Declara três matrizes 3x3
+    int m3[3][3]; // Matriz que vai guardar o resultado da soma
+    int m1[3][3] = { // Primeira matriz, já inicializada
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
-    int m2[3][3] = {
+    int m2[3][3] = { // Segunda matriz, já inicializada
         {9, 8, 7},
         {6, 5, 4},
         {3, 2, 1}
     };
 
-    int dim1 = 3, dim2 = 3;
+    int dim1 = 3, dim2 = 3; // Dimensões das matrizes
 
-    for (p=&m1[0][0], q=&m2[0][0], r=&m3[0][0]; p<&m1[0][0]+dim1*dim2; p++, q++, r++){
-        *r = *p + *q;
+    // Usando ponteiros para percorrer as matrizes como se fossem vetores
+    // p aponta para o primeiro elemento de m1
+    // q aponta para o primeiro elemento de m2
+    // r aponta para o primeiro elemento de m3
+    // O loop vai até o último elemento (dim1*dim2 elementos)
+    for (
+        p = &m1[0][0], q = &m2[0][0], r = &m3[0][0]; // Ponteiros começam no início das matrizes
+        p < &m1[0][0] + dim1 * dim2;                 // Enquanto p não passar do fim da matriz
+        p++, q++, r++                                // Avança todos os ponteiros
+    ){
+        *r = *p + *q; // Soma o valor apontado por p e q, armazena no endereço apontado por r
     }
 
-    // Exibir o resultado da soma
+    // Exibe a matriz resultado da soma
     cout << "Matriz soma:" << endl;
     for (int i = 0; i < dim1; i++) {
         for (int j = 0; j < dim2; j++) {
-            cout << m3[i][j] << " ";
+            cout << m3[i][j] << " "; // Imprime cada elemento da matriz soma
         }
-        cout << endl;
+        cout << endl; // Quebra de linha para cada linha da matriz
     }
 
     return 0;
