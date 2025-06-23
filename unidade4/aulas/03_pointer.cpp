50e13302aa77319bc898db2089a77f931cab8d56
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-23 15:09:25 -0300

    feat: implementar funções de soma e inicialização de vetor no arquivo 03_pointer.cpp

--- unidade4/aulas/03_pointer.cpp
+++ unidade4/aulas/03_pointer.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void somar(float *var, float valor){
+    *var += valor;
+}
+
+void iniVetor(float *v){
+    v[0]=0;
+    v[1]=0;
+    v[2]=0;
+    v[3]=0;
+    v[4]=0;
+}
+
 int main(){
+    float num=0;
+    float vetor[5];
+
+    somar(&num, 10);
+    cout << num << endl;
+    cout << endl;
+
+    iniVetor(vetor);
+    for (int i = 0; i < 5; i++){
+        cout << vetor[i] << endl;
+    }
     
-    
+
     return 0;
 }
