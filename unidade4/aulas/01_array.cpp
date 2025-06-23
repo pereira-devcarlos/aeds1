2ffac8dba4f027c532b70ddda95c20931cb686fa
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-23 14:54:11 -0300

    refactor: adicionar comentários explicativos sobre ponteiros

--- unidade4/aulas/01_array.cpp
+++ unidade4/aulas/01_array.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main(){
     int *p; //Declarando o ponteiro
+    // "p" Endereço
+    // "*p" Valor dentro do endereço
 
     int vetor[10]; // Declarando e atribuindo valores ao array
 
     p=vetor; // Ponteiro recebendo o vetor da posição "0"
     *p= 3; // Alterando o valor do vetor
-    
+
     cout << p << endl;  // Exibindo o endereço do vetor
     cout << *p << endl; // Exibindo o valor do vetor
 
