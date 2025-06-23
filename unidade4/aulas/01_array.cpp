a2f55f0dae737c99da191ff9599de888fc41b016
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-23 14:44:35 -0300

    refactor: reorganizar e adicionar comentários no código do array

--- unidade4/aulas/01_array.cpp
+++ unidade4/aulas/01_array.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main(){
-    int *p;
-    int vetor[10];
-    
+    int *p; //Declarando o ponteiro
+
+    int vetor[10]; // Declarando e atribuindo valores ao array
+    vetor[0]= 5;
+    vetor[1]= 2;
+
+    p=vetor; // Ponteiro recebendo o vetor da posição "0"
+    cout << p << endl;  // Exibindo o endereço do vetor
+    cout << *p << endl; // Exibindo o valor do vetor
+
+    p++; // Passando o ponteiro para a posição seguinte
+    cout << p << endl;  // Exibindo o endereço do vetor
+    cout << *p << endl; // Exibindo o valor do vetor
 
     return 0;
 }
