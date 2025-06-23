e9777df7fed6782227249f8d0208a4bb100de418
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-23 14:47:45 -0300

    feat: inicializar valores do vetor no código do array

--- unidade4/aulas/01_array.cpp
+++ unidade4/aulas/01_array.cpp
@@ -6,14 +6,16 @@ int main(){
     int *p; //Declarando o ponteiro
 
     int vetor[10]; // Declarando e atribuindo valores ao array
-    vetor[0]= 5;
-    vetor[1]= 2;
 
     p=vetor; // Ponteiro recebendo o vetor da posição "0"
+    *p= 3; // Alterando o valor do vetor
+    
     cout << p << endl;  // Exibindo o endereço do vetor
     cout << *p << endl; // Exibindo o valor do vetor
 
     p++; // Passando o ponteiro para a posição seguinte
+    *p= 5; // Alterando o valor do vetor
+
     cout << p << endl;  // Exibindo o endereço do vetor
     cout << *p << endl; // Exibindo o valor do vetor
 
